a6429fcc5af253a8ac98be5b0d828aaeeeef1144
Alexandre Giard <giard.alexandre@gmail.com>
2024-06-12 00:05:17 -0400

    chore: formatting

--- src/Fonts.Avalonia.JetBrainsMono/AppBuilderExtension.cs
+++ src/Fonts.Avalonia.JetBrainsMono/AppBuilderExtension.cs
@@ -3,11 +3,9 @@ using Avalonia;
 namespace Fonts.Avalonia.JetbrainsMono;
 
 public static class AppBuilderExtension {
-	public static AppBuilder WithJetBrainsMonoFont(this AppBuilder appBuilder)
-	{
-		return appBuilder.ConfigureFonts(fontManager =>
-		{
-			fontManager.AddFontCollection(new JetBrainsMonoFontCollection());
-		});
-	}
+    public static AppBuilder WithJetBrainsMonoFont(this AppBuilder appBuilder) {
+        return appBuilder.ConfigureFonts(fontManager => {
+            fontManager.AddFontCollection(new JetBrainsMonoFontCollection());
+        });
+    }
 }
--- src/Fonts.Avalonia.JetBrainsMono/JetBrainsMonoFontCollection.cs
+++ src/Fonts.Avalonia.JetBrainsMono/JetBrainsMonoFontCollection.cs
@@ -3,6 +3,6 @@ using Avalonia.Media.Fonts;
 namespace Fonts.Avalonia.JetbrainsMono;
 
 public class JetBrainsMonoFontCollection : EmbeddedFontCollection {
-	public JetBrainsMonoFontCollection() : base(new Uri("fonts:JetBrainsMono", UriKind.Absolute),
-		new Uri("avares://Fonts.Avalonia.JetbrainsMono/Assets", UriKind.Absolute)) { }
+    public JetBrainsMonoFontCollection() : base(new Uri("fonts:JetBrainsMono", UriKind.Absolute),
+        new Uri("avares://Fonts.Avalonia.JetbrainsMono/Assets", UriKind.Absolute)) { }
 }
