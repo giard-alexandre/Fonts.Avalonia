77da326041e56d6f55a49b31fb3dbd2146df3a40
Alexandre Giard <giard.alexandre@gmail.com>
2024-06-25 00:42:26 -0500

    feat(sample): add grid

--- samples/Fonts.Avalonia.Samples/MainWindow.axaml.cs
+++ samples/Fonts.Avalonia.Samples/MainWindow.axaml.cs
@@ -1,9 +1,13 @@
 using Avalonia.Controls;
 
+using Fonts.Avalonia.Samples.Models;
+
 namespace Fonts.Avalonia.Samples;
 
 public partial class MainWindow : Window {
     public MainWindow() {
         InitializeComponent();
+
+        DataGrid1.ItemsSource = Countries.All;
     }
 }
