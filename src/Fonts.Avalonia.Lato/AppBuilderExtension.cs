d4315cecb4763ded9d8474af28394a79226cf212
Alexandre Giard <giard.alexandre@gmail.com>
2024-10-21 21:28:01 -0500

    fix: lato appbuilder function name

--- src/Fonts.Avalonia.Lato/AppBuilderExtension.cs
+++ src/Fonts.Avalonia.Lato/AppBuilderExtension.cs
@@ -3,7 +3,7 @@ using Avalonia;
 namespace Fonts.Avalonia.Lato;
 
 public static class AppBuilderExtension {
-    public static AppBuilder WithJetBrainsMonoFont(this AppBuilder appBuilder) {
+    public static AppBuilder WithLatoFont(this AppBuilder appBuilder) {
         return appBuilder.ConfigureFonts(fontManager => {
             fontManager.AddFontCollection(new LatoFontCollection());
         });
