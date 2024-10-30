5b38b5dcfe63b75a8bccb566677436fadde53013
Alexandre Giard <giard.alexandre@gmail.com>
2024-10-30 01:00:15 -0500

    feat: Iosevka demo

--- samples/Fonts.Avalonia.Samples/Program.cs
+++ samples/Fonts.Avalonia.Samples/Program.cs
@@ -3,6 +3,7 @@
 using System;
 
 using Fonts.Avalonia.CascadiaCode;
+using Fonts.Avalonia.Iosevka;
 using Fonts.Avalonia.JetBrainsMono;
 using Fonts.Avalonia.Lato;
 using Fonts.Avalonia.Oswald;
@@ -18,5 +19,5 @@ class Program {
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp() =>
-        AppBuilder.Configure<App>().UsePlatformDetect().WithCascadiaCodeFont().LogToTrace();
+        AppBuilder.Configure<App>().UsePlatformDetect().WithIosevkaFont().LogToTrace();
 }
